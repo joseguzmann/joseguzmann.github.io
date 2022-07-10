e04ac0e5d6053e6d4eeab51b38d50ae41c76a9d9
Jose Guzman <jg175415@gmail.com>
2022-07-10 11:02:17 -0500

    feat: menu shadow

--- js/scroll-debounce.js
+++ js/scroll-debounce.js
@@ -8,3 +8,29 @@ let debounced = _.debounce(storeScroll, 250, {'leading': true, 'trailing': true}
 document.addEventListener('scroll', debounced);
 
 storeScroll();
+
+//--------------------------------------------------------------
+
+// let lastTop = window.scrollY;
+
+// const storeScroll = () => {
+//     console.log("LastTop: " + lastTop);
+//     console.log("scrollY: " + window.scrollY);
+//     // console.log(window.scrollY - lastTop)
+//     // console.log(window.innerHeight); 632.7  
+//     if(window.scrollY > lastTop && lastTop < 632.9) {
+//         console.log("Abajo Sombra on");
+//         document.documentElement.dataset.scroll = 1;
+//     } else if(window.scrollY <= lastTop && window.scrollY < 632.7) {
+//         console.log("Arriba Sombra off");
+//         document.documentElement.dataset.scroll = 0;
+//     }
+//     lastTop = window.scrollY;
+// };
+
+// let debounced = _.debounce(storeScroll, 250, {'leading': true, 'trailing': true});
+
+
+// document.addEventListener('scroll', debounced);
+
+// storeScroll();
